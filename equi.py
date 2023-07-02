f3c3f1dd4f664b44bd914dd4f00377347674f650
SruthiBijuGIT <128403226+SruthiBijuGIT@users.noreply.github.com>
2023-07-02 09:51:03 -0700

    Create equi.py

---
+++ equi.py
@@ -0,0 +1,8 @@
+# Write a Python program to check whether a given triangle is equilateral or not 
+x = int(input())
+y = int(input())
+z = int(input())
+if (x==y)and (x==z):
+  print("Equilateral Triangle")
+else:
+  print("Not an Equilateral triangle")
